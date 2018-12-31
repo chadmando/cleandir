1297fabcde13a503c8ef5aa4241a75365c70d7cb
Chad Miars <gobu99@gmail.com>
2018-12-31 12:43:12 -0600

    source file and readme

---
+++ cleandir/README.md
@@ -0,0 +1,11 @@
+_Disclaimer: This is a work in progress.  Use at your own risk.git_
+
+# Cleandir
+
+A directory clean-up script written in pure python.
+
+## What does it do?
+cleandir attempts to organize a folder by grouping the files
+into subfolders based on their file type / extension.
+
+##
---
+++ cleandir/cleandir.py
@@ -0,0 +1,63 @@
+import os
+import shutil
+
+
+def main():
+    path = str(get_path())
+    files = list_dir(path)
+    ext = get_extensions(files)
+    make_folders(path, ext)
+    copy_files(files, ext, path)
+
+
+def get_path():
+    path = input('enter an absolute path to clean (default={}): '.format(os.getcwd()))
+    if os.path.isdir(path):
+        return path
+    else:
+        print('This is not a valid path. Please try again.')
+        get_path()
+
+
+def list_dir(path):
+    in_dir = os.listdir(path)
+    files_in_dir = []
+    for item in in_dir:
+        if os.path.isfile(os.path.join(path, item)):
+            files_in_dir.append(item)
+
+    return files_in_dir
+
+
+def get_extensions(list_of_files):
+    extensions = set()
+    for file in list_of_files:
+        holder = file.partition('.')
+        if holder[2] is not False and len(holder[2]) < 5:
+            extensions.add(str(holder[2]).lower().strip())
+
+    return extensions
+
+
+def make_folders(path, subfolders):
+    for item in subfolders:
+        if not os.path.isdir(os.path.join(path, item)):
+            os.mkdir(os.path.join(path, item))
+            print('created folder in path {}'.format(os.path.join(path, item)))
+
+
+def copy_files(files, ext_list, path):
+    for file in files:
+        base, sep, ext = file.partition('.')
+        ext = str(ext).lower().strip()
+        print(ext)
+        if ext in ext_list:
+            try:
+                shutil.copy2(os.path.join(path, file), os.path.join(path, str(ext).l/ower().strip(), file),
+                             follow_symlinks=False)
+            except shutil.SameFileError:
+                print('an error occurred trying to move {}'.format(os.path.join(path, file)))
+
+
+if __name__ == '__main__':
+    main()
