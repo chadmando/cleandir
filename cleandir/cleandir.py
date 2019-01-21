e83e6f1609a80b8bdd036ad4c6ce0bf8e642c5cc
Chad Miars <gobu99@gmail.com>
2019-01-21 14:40:12 -0600

    refactor get_path function
    needed a Path object returned also added exit code

--- cleandir/cleandir.py
+++ cleandir/cleandir.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 
 def main():
-    path = str(get_path())
+    path = get_path()
     files = list_dir(path)
     ext = get_extensions(files)
     make_folders(path, ext)
@@ -12,13 +12,16 @@ def main():
 
 
 def get_path():
-    path = input('enter an absolute path to clean (default={}): '.format(Path.cwd()))
-    if Path.is_dir(path) and not 'exit':
-        return path
-    else:
-        print('This is not a valid path. Please try again.')
-        get_path()
+    input_path = input('enter an absolute path to clean (default={}): '.format(Path.cwd()))
+    if input_path is not 'exit':
 
+        if path.is_dir() and not 'exit':
+            return path
+        else:
+            print('This is not a valid path. Please try again.')
+            get_path()
+    else:
+        exit(code=0)
 
 def list_dir(path):
     in_dir = Path.iterdir(path)
